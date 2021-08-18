b12d9105b2ca3c02db38cceff0432aa367e0f225
Bence <biharibence01@gmail.com>
2021-08-19 00:32:53 +0200

    Add seed data and route template.

---
+++ TodoApp.WebApplication/Models/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApp.WebApplication.Data;
+
+namespace TodoApp.WebApplication.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new TodoAppContext(serviceProvider.GetRequiredService< DbContextOptions<TodoAppContext>>());
+
+            if (context.TodoItems.Any())
+            {
+                return;
+            }
+
+            var todoItemList = new List<TodoItem>();
+            var random = new Random();
+
+            for (var i = 0; i < 100; i++)
+            {
+                todoItemList.Add(
+                    new TodoItem
+                    {
+                        Message = $"Task {i+1}",
+                        IsDone = random.Next(2)<1
+                    }
+                );
+            }
+
+            context.TodoItems.AddRange(todoItemList);
+            context.SaveChanges();
+        }
+    }
+}
--- TodoApp.WebApplication/Models/TodoItem.cs
+++ TodoApp.WebApplication/Models/TodoItem.cs
@@ -1,9 +1,14 @@
-﻿namespace TodoApp.WebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.WebApplication.Models
 {
     public class TodoItem
     {
         public int Id { get; set; }
+
         public string Message { get; set; }
+
+        [Display(Name = "Done")]
         public bool IsDone { get; set; }
     }
 }
